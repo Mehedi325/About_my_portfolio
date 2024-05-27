c57e0afe4ca5a45459c127f2405eaf94ecc253b2
Mehedi Hasan Masum <132672069+Mehedi325@users.noreply.github.com>
2024-05-27 19:27:58 +0600

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
     <header id="home">
       <div className="container header__container">
         <h5>Hi, There I'm</h5>
-        <h1>Md.Mehedi Hasan Masum</h1>
-        <h2 className="text-light">Front-End Developer & SEO Expert</h2>
+        <h1>Md. Mehedi Hasan Masum</h1>
+        <h2 className="text-light">Front-End Web Developer & <br/> SEO Expert.</h2>
         <CTA />
         <a href="#contact" className="scroll__down">
           Scroll Down
